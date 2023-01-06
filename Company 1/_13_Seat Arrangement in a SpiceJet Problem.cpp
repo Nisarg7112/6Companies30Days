efde6b20021a6822657ad04d69150d8adfc91d9b
Nisarg Shah <109804208+Nisarg7112@users.noreply.github.com>
2023-01-06 18:14:11 +0530

    Create _13_Seat Arrangement in a SpiceJet Problem.cpp

---
+++ Company 1/_13_Seat Arrangement in a SpiceJet Problem.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        if(n==1){
+            return 1;
+        }
+        // double y=n;
+        double x =1/double(2);
+
+        return x;
+    }
+};
